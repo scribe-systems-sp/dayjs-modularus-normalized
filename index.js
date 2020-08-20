5c5b3bc3c5b08a1aeabf87d74f62047fac840b05
Adam Raźniewski <adam@razniewski.eu>
2020-08-20 16:33:34 +0200

    dayjs normalized

---
+++ index.js
@@ -0,0 +1,20 @@
+
+export default (option, dayjsClass, dayjsFactory) => {
+
+    // Set timezone to guessed one
+    if(!window.localStorage.scribeTimeZone) {
+        let guessed = dayjsFactory.tz.guess()
+        window.localStorage.scribeTimeZone = guessed
+    }
+
+    dayjsFactory.normalized = function (date) {
+        const cfg = { date, args: arguments }
+        let tzLocal = window.localStorage.scribeTimeZone
+        try {
+            return new dayjsClass(cfg).tz(tzLocal)
+        } catch(err) {
+            let guessed = dayjsFactory.tz.guess()
+            return new dayjsClass(cfg).tz(guessed)
+        }
+    }
+}
